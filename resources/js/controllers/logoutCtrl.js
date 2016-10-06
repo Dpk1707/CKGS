d4900259f6fb92b962d36ed2a661f5571b3385b7
pk1988 <phanikumar.narina@magikminds.com>
2016-10-06 10:44:05 +0530

    Added Rating Logic

--- resources/js/controllers/logoutCtrl.js
+++ resources/js/controllers/logoutCtrl.js
@@ -15,7 +15,33 @@ controller('navCtrl', ['$rootScope', '$scope', '$window', '$state', '$http', '$t
             templateUrl: 'views/rateApp.html',
             size: size,
             controller: function($scope,$uibModalInstance){
-
+                $scope.close = function(){
+                    $uibModalInstance.dismiss('cancel');
+                }
+                $scope.rateCKGS = function(val){
+                    if (val == "" || val == undefined) {
+                        swal("Cancelled", "Please rate the app!", "error");
+                        return;
+                    }
+                    else{
+                        var data = {};
+                        var rating= {};
+                        rating.ValidationKey = CONSTANTS.testValidationKey;
+                        rating.WebRefNo = localStorageService.get('WRF');
+                        rating.DateOfBirth = localStorageService.get('DOB');
+                        rating.Passport = localStorageService.get('PassNo');
+                        rating.Rating = val;
+                        data.CKGSDataRequest = rating;
+                        var headers = restServices.getHeaders();
+                        var config = {};
+                        config.headers = headers;
+                        var url = CONSTANTS.appRating;
+                        restServices.restPutType(url,data,config,function(status,res) {
+                            $uibModalInstance.dismiss('cancel');
+                            swal("Success!", ""+res.data.CKGSDataResponse.StatusMsg+"", "success");
+                        });
+                    }
+                }
             }
         });
     }
